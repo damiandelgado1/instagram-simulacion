8febe730b17712d4bb6b6f67317eeda8c9c89838
Damian <damian.delgadogallardo01@gmail.com>
2026-05-02 12:20:49 -0400

    Add feature Like post

--- instagram/urls.py
+++ instagram/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from .views import HomeView, RegisterView, LoginView, ContactView, LegalView, logout_view, ProfileListView, ProfileDetailView, ProfileUpdateView
-from post.views import PostCreateView
+from post.views import PostCreateView, PostDetailView, like_post
 
 
 urlpatterns = [
@@ -16,6 +16,8 @@ urlpatterns = [
     path('profile/<pk>/', ProfileDetailView.as_view(), name="profile_detail"),
     path('profile/update/<pk>/', ProfileUpdateView.as_view(), name="profile_update"),
     path('post/create/', PostCreateView.as_view(), name="post_create"),
+    path('post/<pk>/', PostDetailView.as_view(), name="post_detail"),
+    path('post/like/<pk>/', like_post, name="post_like"),
     path('legal/', LegalView.as_view(), name="legal"),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- post/views.py
+++ post/views.py
@@ -1,8 +1,10 @@
 from django.views.generic.edit import CreateView
 from django.contrib import messages
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
+from django.http import HttpResponseRedirect
+from django.views.generic.detail import DetailView
 from post.models import Post
 from .forms import PostCreate
 
@@ -16,7 +18,25 @@ class PostCreateView(CreateView):
 
     def form_valid(self, form):
         form.instance.user = self.request.user
-
         messages.add_message(self.request, messages.SUCCESS, "Publicacion Creada")
-
         return super(PostCreateView, self).form_valid(form)
+
+
+@method_decorator(login_required, name="dispatch")
+class PostDetailView(DetailView):
+    template_name = "post/post_detail.html"
+    model = Post
+    context_object_name = "post"
+
+
+@login_required
+def like_post(request, pk):
+    post = Post.objects.get(pk=pk)
+
+    if request.user in post.likes.all():
+        messages.add_message(request, messages.INFO, f"Ya le das Like a la Publicacion")
+        post.likes.remove(request.user)
+    else:
+        post.likes.add(request.user)
+        messages.add_message(request, messages.INFO, "Like a la Publicacion")
+        return HttpResponseRedirect(reverse('post_detail', args=[pk]))
